f3ac1347a63a4587143e743f6946cd0cc8af97b2
Clem <95104833+JustClemLeBref@users.noreply.github.com>
2022-03-04 13:24:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,335 @@
 import pygame, sys
-from os import walk
+from settings import * 
+
 
 # Pygame setup
 pygame.init()
-
-screen_width = 1440
-screen_height = 1000
-
-
+screen = pygame.display.set_mode((screen_width,screen_height))
 clock = pygame.time.Clock()
 
-white = (255, 255, 255)
-red = (255, 0, 0)
-black = (0, 0, 0)
+#________________________support_________________________
+
+def import_folder(path):
+	surface_list = []
+
+	for _,__,img_files in walk(path):
+		for image in img_files:
+			full_path = path + '/' + image
+			image_surf = pygame.image.load(full_path).convert_alpha()
+			surface_list.append(image_surf)
+
+	return surface_list
+
+#_______________________________________________________steeings__________
+level_map = [
+'                            ',
+'                            ',
+'                            ',
+' XX    XXX            XX    ',
+' XX P                       ',
+' XXXX         XX         XX ',
+' XXXX       XX              ',
+' XX    X  XXXX    XX  XX    ',
+'       X  XXXX    XX  XXX   ',
+'    XXXX  XXXXXX  XX  XXXX  ',
+'XXXXXXXX  XXXXXX  XX  XXXX  ']
+
+tile_size = 64
+screen_width = 1200
+screen_height = len(level_map) * tile_size
+
+#__________________________________________particles__________________________
+
+class ParticleEffect(pygame.sprite.Sprite):
+	def __init__(self,pos,type):
+		super().__init__()
+		self.frame_index = 0
+		self.animation_speed = 0.5
+		if type == 'jump':
+			self.frames = import_folder('../graphics/character/dust_particles/jump')
+		if type == 'land':
+			self.frames = import_folder('../graphics/character/dust_particles/land')
+		self.image = self.frames[self.frame_index]
+		self.rect = self.image.get_rect(center = pos)
+
+	def animate(self):
+		self.frame_index += self.animation_speed
+		if self.frame_index >= len(self.frames):
+			self.kill()
+		else:
+			self.image = self.frames[int(self.frame_index)]
+
+	def update(self,x_shift):
+		self.animate()
+		self.rect.x += x_shift
+
+
+#____________________________________________player_________________
+
+
+class Player(pygame.sprite.Sprite):
+	def __init__(self,pos,surface,create_jump_particles):
+		super().__init__()
+		self.import_character_assets()
+		self.frame_index = 0
+		self.animation_speed = 0.15
+		self.image = self.animations['idle'][self.frame_index]
+		self.rect = self.image.get_rect(topleft = pos)
+		
+		# dust particles 
+		self.import_dust_run_particles()
+		self.dust_frame_index = 0
+		self.dust_animation_speed = 0.15
+		self.display_surface = surface
+		self.create_jump_particles = create_jump_particles
+
+		# player movement
+		self.direction = pygame.math.Vector2(0,0)
+		self.speed = 8
+		self.gravity = 0.8
+		self.jump_speed = -16
+
+		# player status
+		self.status = 'idle'
+		self.facing_right = True
+		self.on_ground = False
+		self.on_ceiling = False
+		self.on_left = False
+		self.on_right = False
+
+	def import_character_assets(self):
+		character_path = '../graphics/character/'
+		self.animations = {'idle':[],'run':[],'jump':[],'fall':[]}
+
+		for animation in self.animations.keys():
+			full_path = character_path + animation
+			self.animations[animation] = import_folder(full_path)
+
+	def import_dust_run_particles(self):
+		self.dust_run_particles = import_folder('../graphics/character/dust_particles/run')
+
+	def animate(self):
+		animation = self.animations[self.status]
+
+		# loop over frame index 
+		self.frame_index += self.animation_speed
+		if self.frame_index >= len(animation):
+			self.frame_index = 0
+
+		image = animation[int(self.frame_index)]
+		if self.facing_right:
+			self.image = image
+		else:
+			flipped_image = pygame.transform.flip(image,True,False)
+			self.image = flipped_image
+
+		# set the rect
+		if self.on_ground and self.on_right:
+			self.rect = self.image.get_rect(bottomright = self.rect.bottomright)
+		elif self.on_ground and self.on_left:
+			self.rect = self.image.get_rect(bottomleft = self.rect.bottomleft)
+		elif self.on_ground:
+			self.rect = self.image.get_rect(midbottom = self.rect.midbottom)
+		elif self.on_ceiling and self.on_right:
+			self.rect = self.image.get_rect(topright = self.rect.topright)
+		elif self.on_ceiling and self.on_left:
+			self.rect = self.image.get_rect(topleft = self.rect.topleft)
+		elif self.on_ceiling:
+			self.rect = self.image.get_rect(midtop = self.rect.midtop)
+
+	def run_dust_animation(self):
+		if self.status == 'run' and self.on_ground:
+			self.dust_frame_index += self.dust_animation_speed
+			if self.dust_frame_index >= len(self.dust_run_particles):
+				self.dust_frame_index = 0
+
+			dust_particle = self.dust_run_particles[int(self.dust_frame_index)]
+
+			if self.facing_right:
+				pos = self.rect.bottomleft - pygame.math.Vector2(6,10)
+				self.display_surface.blit(dust_particle,pos)
+			else:
+				pos = self.rect.bottomright - pygame.math.Vector2(6,10)
+				flipped_dust_particle = pygame.transform.flip(dust_particle,True,False)
+				self.display_surface.blit(flipped_dust_particle,pos)
+
+	def get_input(self):
+		keys = pygame.key.get_pressed()
+
+		if keys[pygame.K_RIGHT]:
+			self.direction.x = 1
+			self.facing_right = True
+		elif keys[pygame.K_LEFT]:
+			self.direction.x = -1
+			self.facing_right = False
+		else:
+			self.direction.x = 0
+
+		if keys[pygame.K_SPACE] and self.on_ground:
+			self.jump()
+			self.create_jump_particles(self.rect.midbottom)
+
+	def get_status(self):
+		if self.direction.y < 0:
+			self.status = 'jump'
+		elif self.direction.y > 1:
+			self.status = 'fall'
+		else:
+			if self.direction.x != 0:
+				self.status = 'run'
+			else:
+				self.status = 'idle'
+
+	def apply_gravity(self):
+		self.direction.y += self.gravity
+		self.rect.y += self.direction.y
+
+	def jump(self):
+		self.direction.y = self.jump_speed
+
+	def update(self):
+		self.get_input()
+		self.get_status()
+		self.animate()
+		self.run_dust_animation()
+		
+#_________________________________level______________
+
+
+class Level:
+	def __init__(self,level_data,surface):
+		
+		# level setup
+		self.display_surface = surface 
+		self.setup_level(level_data)
+		self.world_shift = 0
+		self.current_x = 0
+
+		# dust 
+		self.dust_sprite = pygame.sprite.GroupSingle()
+		self.player_on_ground = False
+
+	def create_jump_particles(self,pos):
+		if self.player.sprite.facing_right:
+			pos -= pygame.math.Vector2(10,5)
+		else:
+			pos += pygame.math.Vector2(10,-5)
+		jump_particle_sprite = ParticleEffect(pos,'jump')
+		self.dust_sprite.add(jump_particle_sprite)
+
+	def get_player_on_ground(self):
+		if self.player.sprite.on_ground:
+			self.player_on_ground = True
+		else:
+			self.player_on_ground = False
+
+	def create_landing_dust(self):
+		if not self.player_on_ground and self.player.sprite.on_ground and not self.dust_sprite.sprites():
+			if self.player.sprite.facing_right:
+				offset = pygame.math.Vector2(10,15)
+			else:
+				offset = pygame.math.Vector2(-10,15)
+			fall_dust_particle = ParticleEffect(self.player.sprite.rect.midbottom - offset,'land')
+			self.dust_sprite.add(fall_dust_particle)
+
+	def setup_level(self,layout):
+		self.tiles = pygame.sprite.Group()
+		self.player = pygame.sprite.GroupSingle()
+
+		for row_index,row in enumerate(layout):
+			for col_index,cell in enumerate(row):
+				x = col_index * tile_size
+				y = row_index * tile_size
+				
+				if cell == 'X':
+					tile = Tile((x,y),tile_size)
+					self.tiles.add(tile)
+				if cell == 'P':
+					player_sprite = Player((x,y),self.display_surface,self.create_jump_particles)
+					self.player.add(player_sprite)
+
+	def scroll_x(self):
+		player = self.player.sprite
+		player_x = player.rect.centerx
+		direction_x = player.direction.x
+
+		if player_x < screen_width / 4 and direction_x < 0:
+			self.world_shift = 8
+			player.speed = 0
+		elif player_x > screen_width - (screen_width / 4) and direction_x > 0:
+			self.world_shift = -8
+			player.speed = 0
+		else:
+			self.world_shift = 0
+			player.speed = 8
+
+	def horizontal_movement_collision(self):
+		player = self.player.sprite
+		player.rect.x += player.direction.x * player.speed
+
+		for sprite in self.tiles.sprites():
+			if sprite.rect.colliderect(player.rect):
+				if player.direction.x < 0: 
+					player.rect.left = sprite.rect.right
+					player.on_left = True
+					self.current_x = player.rect.left
+				elif player.direction.x > 0:
+					player.rect.right = sprite.rect.left
+					player.on_right = True
+					self.current_x = player.rect.right
+
+		if player.on_left and (player.rect.left < self.current_x or player.direction.x >= 0):
+			player.on_left = False
+		if player.on_right and (player.rect.right > self.current_x or player.direction.x <= 0):
+			player.on_right = False
+
+	def vertical_movement_collision(self):
+		player = self.player.sprite
+		player.apply_gravity()
+
+		for sprite in self.tiles.sprites():
+			if sprite.rect.colliderect(player.rect):
+				if player.direction.y > 0: 
+					player.rect.bottom = sprite.rect.top
+					player.direction.y = 0
+					player.on_ground = True
+				elif player.direction.y < 0:
+					player.rect.top = sprite.rect.bottom
+					player.direction.y = 0
+					player.on_ceiling = True
+
+		if player.on_ground and player.direction.y < 0 or player.direction.y > 1:
+			player.on_ground = False
+		if player.on_ceiling and player.direction.y > 0.1:
+			player.on_ceiling = False
+
+	def run(self):
+		# dust particles 
+		self.dust_sprite.update(self.world_shift)
+		self.dust_sprite.draw(self.display_surface)
+
+		# level tiles
+		self.tiles.update(self.world_shift)
+		self.tiles.draw(self.display_surface)
+		self.scroll_x()
+
+
+		# player
+		self.player.update()
+		self.horizontal_movement_collision()
+		self.get_player_on_ground()
+		self.vertical_movement_collision()
+		self.create_landing_dust()
+		self.player.draw(self.display_surface)
+
+
+
+
+
+
+
+
+level = Level(level_map,screen)
 
 while True:
 	for event in pygame.event.get():
@@ -20,7 +337,8 @@ while True:
 			pygame.quit()
 			sys.exit()
 	
-
+	screen.fill('black')
+	level.run()
 
 	pygame.display.update()
 	clock.tick(60)
